648f58693273b776d01dcc83cba80c4639ea1f87
Himanshu <himanshu.singh@bdiplus.com>
2023-06-15 23:57:40 +0530

    update: @Primary and @Qualifier

--- src/main/java/com/example/springboot/controller/ApplicationController.java
+++ src/main/java/com/example/springboot/controller/ApplicationController.java
@@ -7,13 +7,26 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.springboot.service.Coach;
 
+
+/**
+ * Note: 
+ * 1. Alternative way to sort multiple implemnetation is to use @Primary
+ * 2. You can mark only class as @Primary
+ * 3. If defined both @Qualifier has highest priority than @Primary
+ */
+
 @RestController
 public class ApplicationController {
 
     private Coach myCoach;
 
     @Autowired
-    public void setCoach(@Qualifier("cricketCoach")Coach myCoach){
+    public void setCoach(@Qualifier("tennisCoach")Coach myCoach){
+        this.myCoach=myCoach;
+    }
+
+    @Autowired
+    public ApplicationController(Coach myCoach){
         this.myCoach=myCoach;
     }
 
